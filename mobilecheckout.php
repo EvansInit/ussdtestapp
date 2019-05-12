a275c84fdb500a3147bea1738138252714050ca8
Evans Init <evansinit@gmail.com>
2019-05-12 12:59:22 +0300

    created an index.php

---
+++ mobilecheckout.php
@@ -0,0 +1,46 @@
+<?php
+//import SDK
+require 'vendor/autoload.php';
+use AfricasTalking\SDK\AfricasTalking;
+
+// Set your app credentials
+$username   = "sandbox";
+$apikey     = "Your_api_key";
+
+// Initialize the SDK
+$AT         = new AfricasTalking($username, $apikey);
+
+// Get the payments service
+$payments   = $AT->payments();
+
+// Set the name of your Africa's Talking payment product
+$productName  = "your_product_name";
+
+// Set the phone number you want to send to in international format
+$phoneNumber  = "+254711XXXYYY";
+
+// Set The 3-Letter ISO currency code and the checkout amount
+$currencyCode = "KES";
+$amount       = 100.50;
+
+// Set any metadata that you would like to send along with this request.
+// This metadata will be included when we send back the final payment notification
+$metadata = [
+    "agentId"   => "654",
+    "productId" => "321"
+];
+
+// Thats it, hit send and we'll take care of the rest.
+try {
+    $result = $payments->mobileCheckout([
+        "productName"  => $productName,
+        "phoneNumber"  => $phoneNumber,
+        "currencyCode" => $currencyCode,
+        "amount"       => $amount,
+        "metadata"     => $metadata
+    ]);
+
+    print_r($result);
+} catch(Exception $e) {
+    echo "Error: ".$e->getMessage();
+}
